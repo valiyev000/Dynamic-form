3656edf99badccbda5bfc2d80d1fb290390efc74
valiyev000 <veliev1203@gmail.com>
2023-06-15 13:40:32 +0400

    new update

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,48 @@
 import { useState } from 'react'
 import './App.scss'
 import FirstForm from './components/FirstForm'
-import FourthForm from './components/FourthForm'
 import SecondForm from './components/SecondForm'
+import FourthForm from './components/FourthForm'
 import ThirdForm from './components/ThirdForm'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+
+
 
 function App() {
 
   const [pageCount, setPageCount] = useState(1)
 
-  console.log(pageCount);
+  console.log(pageCount,"App render");
   return (
     <main>
       <div className="header">Get a project quote</div>
       <div className="description">Please fill the form below to receive a quote for your project. Feel free to add as much detail as needed.</div>
       <div className="window">
         <div className="progressBar">
-          <div className="pageCounter" style={{background:(pageCount>0) ? "#4A3AFF" : "#EFF0F6",color:(pageCount>0) ? "#FFFFFF" : "#6F6C90"}}>1</div>
+          <div className="pageCounter" style={{ background: (pageCount > 0) ? "#4A3AFF" : "#EFF0F6", color: (pageCount > 0) ? "#FFFFFF" : "#6F6C90" }}>1</div>
           <div className="range">
-            <div className="rangeInner" style={{width:(pageCount>1 ? "100%" :(pageCount===1)? "50%" : "0%")}}></div>
+            <div className="rangeInner" style={{ width: (pageCount > 1 ? "100%" : (pageCount === 1) ? "50%" : "0%") }}></div>
           </div>
-          <div className="pageCounter" style={{background:(pageCount>1) ? "#4A3AFF" : "#EFF0F6",color:(pageCount>1) ? "#FFFFFF" : "#6F6C90"}}>2</div>
+          <div className="pageCounter" style={{ background: (pageCount > 1) ? "#4A3AFF" : "#EFF0F6", color: (pageCount > 1) ? "#FFFFFF" : "#6F6C90" }}>2</div>
           <div className="range">
-            <div className="rangeInner" style={{width:(pageCount>2 ? "100%" :(pageCount===2)? "50%" : "0%")}}></div>
+            <div className="rangeInner" style={{ width: (pageCount > 2 ? "100%" : (pageCount === 2) ? "50%" : "0%") }}></div>
           </div>
-          <div className="pageCounter" style={{background:(pageCount>2) ? "#4A3AFF" : "#EFF0F6",color:(pageCount>2) ? "#FFFFFF" : "#6F6C90"}}>3</div>
+          <div className="pageCounter" style={{ background: (pageCount > 2) ? "#4A3AFF" : "#EFF0F6", color: (pageCount > 2) ? "#FFFFFF" : "#6F6C90" }}>3</div>
           <div className="range">
-            <div className="rangeInner" style={{width:(pageCount>3 ? "100%" :(pageCount===3)? "50%" : "0%")}}></div>
+            <div className="rangeInner" style={{ width: (pageCount > 3 ? "100%" : (pageCount === 3) ? "50%" : "0%") }}></div>
           </div>
-          <div className="pageCounter" style={{background:(pageCount>3) ? "#4A3AFF" : "#EFF0F6",color:(pageCount>3) ? "#FFFFFF" : "#6F6C90"}}>4</div>
+          <div className="pageCounter" style={{ background: (pageCount > 3) ? "#4A3AFF" : "#EFF0F6", color: (pageCount > 3) ? "#FFFFFF" : "#6F6C90" }}>4</div>
         </div>
         <div className="dynamicWindow">
-          <FirstForm pageCount={pageCount} />
-          <SecondForm pageCount={pageCount} />
-          <ThirdForm pageCount={pageCount} />
-          <FourthForm pageCount={pageCount} />
+          <FirstForm pageCount={pageCount} setPageCount={setPageCount} useFormik={useFormik} Yup={Yup} />
+          <SecondForm pageCount={pageCount} setPageCount={setPageCount} useFormik={useFormik} Yup={Yup} />
+          <ThirdForm pageCount={pageCount} setPageCount={setPageCount} useFormik={useFormik} Yup={Yup} />
+          <FourthForm pageCount={pageCount} setPageCount={setPageCount} useFormik={useFormik} Yup={Yup} />
         </div>
       </div>
-      <button onClick={()=>setPageCount((prev)=>prev+1)}>Next Step</button>
     </main>
   )
 }
 
 export default App
-//! duymenin stylinda qalmisam // sonra formik duzletmek lazimdi
--- src/components/FirstForm.jsx
+++ src/components/FirstForm.jsx
@@ -1,15 +1,52 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import nameIcon from '../images/nameIcon.svg'
 import emailIcon from '../images/emailIcon.svg'
 import phoneIcon from '../images/phoneIcon.svg'
 import companyIcon from '../images/companyIcon.svg'
 
-export default function FirstForm({ pageCount }) {
+export default function FirstForm({ pageCount, useFormik, Yup, setPageCount }) {
+
+    console.log("FirstForm render");
+
+    
+
+    const firstFormValidation = Yup.object().shape(
+        {
+            name: Yup.string("*Only text").required("*Required").min(5, "*Min 5 letter required"),
+            email: Yup.string("*Only text").required("*Required").email("*Not an email"),
+            phone: Yup.string("*Not a number").required("*Required").min(10, "*Min 10").max(10, "Max 10"),
+            company: Yup.string("*Only text").required("*Required")
+        }
+    )
+
+    const formik = useFormik(
+        {
+            initialValues: {
+                name: '',
+                email: '',
+                phone: '',
+                company: '',
+            },
+            validateOnMount: true,
+            onSubmit: (e)=> {
+                console.log(e)
+            },
+            validationSchema: firstFormValidation
+        }
+    )
+
+        if (formik.isValid) {
+            console.log(true);
+        } else {
+            console.log(false)
+        }
+    
+
     return (
         <section className='firstForm form' style={{ display: pageCount === 1 ? "block" : "none" }}>
             <div className="header">Contact details</div>
             <div className="description">Lorem ipsum dolor sit amet consectetur adipisc.</div>
-            <form action="#" method="post">
+            <form action="#" onSubmit={formik.handleSubmit}>
                 <div className="formElement">
                     <label htmlFor="name">Name</label>
                     <div className="inputDiv">
@@ -18,10 +55,14 @@ export default function FirstForm({ pageCount }) {
                             name="name"
                             id="name"
                             placeholder='John Carter'
+                            value={formik.values.name}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
                         />
                         <div className="imgDiv">
                             <img src={nameIcon} alt="nameIcon.svg" />
                         </div>
+                        <small className="errorMessage">{formik.touched.name && formik.errors.name && formik.errors.name}</small>
                     </div>
                 </div>
                 <div className="formElement">
@@ -32,10 +73,14 @@ export default function FirstForm({ pageCount }) {
                             name="email"
                             id="email"
                             placeholder='Email address'
+                            value={formik.values.email}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
                         />
                         <div className="imgDiv">
                             <img src={emailIcon} alt="emailIcon.svg" />
                         </div>
+                        <small className="errorMessage">{formik.touched.email && formik.errors.email && formik.errors.email}</small>
                     </div>
                 </div>
                 <div className="formElement">
@@ -46,11 +91,14 @@ export default function FirstForm({ pageCount }) {
                             name="phone"
                             id="phone"
                             placeholder='(123)4567890'
+                            value={formik.values.phone}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
                         />
                         <div className="imgDiv">
                             <img src={phoneIcon} alt="phoneIcon.svg" />
                         </div>
-
+                        <small className="errorMessage">{formik.touched.phone && formik.errors.phone && formik.errors.phone}</small>
                     </div>
                 </div>
                 <div className="formElement">
@@ -61,12 +109,17 @@ export default function FirstForm({ pageCount }) {
                             name="company"
                             id="company"
                             placeholder='Company name'
+                            value={formik.values.company}
+                            onChange={formik.handleChange}
+                            onBlur={formik.handleBlur}
                         />
                         <div className="imgDiv">
                             <img src={companyIcon} alt="companyIcon.svg" />
                         </div>
+                        <small className="errorMessage">{formik.touched.company && formik.errors.company && formik.errors.company}</small>
                     </div>
                 </div>
+                <input type="submit" value="Submit" onClick={formik.isValid ?()=>setPageCount((prev) => prev + 1):null} className='nextBtn' />
             </form>
         </section>
     )
